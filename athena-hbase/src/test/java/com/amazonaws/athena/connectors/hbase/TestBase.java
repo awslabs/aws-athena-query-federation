ad2e9da764c6212e10980eb4ea78448df2fc203f
Soojin Jeong <soojinj@amazon.com>
2020-03-11 17:24:20 -0400

    Clean up unit tests

--- athena-hbase/src/test/java/com/amazonaws/athena/connectors/hbase/TestBase.java
+++ athena-hbase/src/test/java/com/amazonaws/athena/connectors/hbase/TestBase.java
@@ -7,9 +7,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+<<<<<<< HEAD
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
+=======
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+>>>>>>> Clean up unit tests
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,5 +35,4 @@ public class TestBase {
     protected static final String TEST_TABLE = "test_table";
     protected static final String DEFAULT_SCHEMA = "default";
     protected static final TableName TABLE_NAME = new TableName(DEFAULT_SCHEMA, TEST_TABLE);
-
 }
