95886a0876bd7e7951f79fd0cfc18014c7fbc3bb
Shachar Hurvitz <shurvitz@amazon.com>
2020-04-13 11:07:18 -0400

    Added toArrowType() to convert Elasticsearch data-type to ArrowType.

--- athena-elasticsearch/src/main/java/com/amazonaws/connectors/athena/elasticsearch/ElasticsearchHelper.java
+++ athena-elasticsearch/src/main/java/com/amazonaws/connectors/athena/elasticsearch/ElasticsearchHelper.java
@@ -20,11 +20,54 @@
 package com.amazonaws.connectors.athena.elasticsearch;
 
 import com.amazonaws.athena.connector.lambda.data.SchemaBuilder;
+import org.apache.arrow.vector.types.Types;
+import org.apache.arrow.vector.types.pojo.ArrowType;
 import org.apache.arrow.vector.types.pojo.Schema;
 
 import java.util.LinkedHashMap;
 
 class ElasticsearchHelper {
+
+    /**
+     * toArrowType
+     *
+     * Convert the data type from Elasticsearch to Arrow.
+     *
+     * @param elasticType is the Elasticsearch datatype.
+     * @return an ArrowType corresponding to the Elasticsearch type (default value is a VARCHAR).
+     */
+    public static ArrowType toArrowType(String elasticType)
+    {
+        switch (elasticType) {
+            // case "text":
+            // case "keyword":
+            //    return Types.MinorType.VARCHAR.getType();
+            case "long":
+                return Types.MinorType.BIGINT.getType();
+            case "integer":
+                return Types.MinorType.INT.getType();
+            case "short":
+                return Types.MinorType.SMALLINT.getType();
+            case "byte":
+                return Types.MinorType.TINYINT.getType();
+            case "double":
+            case "scaled_float":
+                return Types.MinorType.FLOAT8.getType();
+            case "float":
+            case "half_float":
+                return Types.MinorType.FLOAT4.getType();
+            case "date":
+            case "date_nanos":
+                return Types.MinorType.DATEMILLI.getType();
+            case "boolean":
+                return Types.MinorType.BIT.getType();
+            case "binary":
+                return Types.MinorType.VARBINARY.getType();
+            default:
+                return Types.MinorType.VARCHAR.getType();
+        }
+    }
+
     /**
      * parseMapping
      *
@@ -35,7 +78,8 @@ class ElasticsearchHelper {
      * @param mapping is the current map of the element in question (e.g. address).
      * @param builder builds the schema at the iteration through the mapping.
      */
-    private static void parseMapping(String prefix, LinkedHashMap<String, Object> mapping, SchemaBuilder builder) {
+    private static void parseMapping(String prefix, LinkedHashMap<String, Object> mapping, SchemaBuilder builder)
+    {
         for (String key : mapping.keySet()) {
             String fieldName = prefix.isEmpty() ? key : prefix + "." + key;
             LinkedHashMap<String, Object> currMapping = (LinkedHashMap<String, Object>) mapping.get(key);
@@ -44,7 +88,7 @@ class ElasticsearchHelper {
                 parseMapping(fieldName, (LinkedHashMap<String, Object>) currMapping.get("properties"), builder);
             }
             else if (currMapping.containsKey("type")) {
-                builder.addStringField(fieldName);
+                builder.addField(fieldName, toArrowType((String) currMapping.get("type")));
             }
         }
     }
@@ -55,9 +99,10 @@ class ElasticsearchHelper {
      * Main parsing method for the GET <index>/_mapping request.
      *
      * @param mapping is the structure that contains the mapping for all elements for the index.
-     * @return returns a Schema derived from the mapping.
+     * @return a Schema derived from the mapping.
      */
-    public static Schema parseMapping(LinkedHashMap<String, Object> mapping) {
+    public static Schema parseMapping(LinkedHashMap<String, Object> mapping)
+    {
         LinkedHashMap<String, String> schema = new LinkedHashMap<>();
         SchemaBuilder builder = SchemaBuilder.newBuilder();
         String fieldName = "";
