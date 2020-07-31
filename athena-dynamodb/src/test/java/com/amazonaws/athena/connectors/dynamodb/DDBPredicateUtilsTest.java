bb864e016e5dc15a70d2c21f3f8ff71b2d2bef0f
Shachar Hurvitz <shurvitz@amazon.com>
2020-07-31 13:33:13 -0400

    Updated Unit Test for aliasColumn.

--- athena-dynamodb/src/test/java/com/amazonaws/athena/connectors/dynamodb/DDBPredicateUtilsTest.java
+++ athena-dynamodb/src/test/java/com/amazonaws/athena/connectors/dynamodb/DDBPredicateUtilsTest.java
@@ -1,3 +1,22 @@
+/*-
+ * #%L
+ * athena-dynamodb
+ * %%
+ * Copyright (C) 2019 - 2020 Amazon Web Services
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.amazonaws.athena.connectors.dynamodb;
 
 import com.amazonaws.athena.connectors.dynamodb.util.DDBPredicateUtils;
@@ -18,9 +37,9 @@ public class DDBPredicateUtilsTest
     private static final Logger logger = LoggerFactory.getLogger(DDBPredicateUtilsTest.class);
 
     @Test
-    public void aliasColumnTest()
+    public void testAliasColumn()
     {
-        logger.info("aliasColumnTest - enter");
+        logger.info("testAliasColumn - enter");
 
         assertEquals("Unexpected alias column value!", "#column_1",
                 DDBPredicateUtils.aliasColumn("column_1"));
@@ -31,6 +50,6 @@ public class DDBPredicateUtilsTest
         assertEquals("Unexpected alias column value!", "#column_1_f3F",
                 DDBPredicateUtils.aliasColumn("column-$1`~!@#$%^&*()-=+[]{}\\|;:'\",.<>/?f3F"));
 
-        logger.info("aliasColumnTest - exit");
+        logger.info("testAliasColumn - exit");
     }
 }
