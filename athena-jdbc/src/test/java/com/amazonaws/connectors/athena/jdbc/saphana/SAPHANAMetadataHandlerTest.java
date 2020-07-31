0e8ec2a4a3dcba69fc397d0c6c8f2c3024abd84f
Harpreet Singh <batrahs@gmail.com>
2020-07-31 05:24:23 +0000

    SAP HANA JDBC Tests

---
+++ athena-jdbc/src/test/java/com/amazonaws/connectors/athena/jdbc/saphana/SAPHANAMetadataHandlerTest.java
@@ -0,0 +1,277 @@
+/*-
+ * #%L
+ * athena-jdbc
+ * %%
+ * Copyright (C) 2019 Amazon Web Services
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.amazonaws.connectors.athena.jdbc.saphana;
+
+import com.amazonaws.athena.connector.lambda.data.BlockAllocator;
+import com.amazonaws.athena.connector.lambda.data.BlockAllocatorImpl;
+import com.amazonaws.athena.connector.lambda.data.BlockUtils;
+import com.amazonaws.athena.connector.lambda.data.FieldBuilder;
+import com.amazonaws.athena.connector.lambda.data.SchemaBuilder;
+import com.amazonaws.athena.connector.lambda.domain.Split;
+import com.amazonaws.athena.connector.lambda.domain.TableName;
+import com.amazonaws.athena.connector.lambda.domain.predicate.Constraints;
+import com.amazonaws.athena.connector.lambda.metadata.GetSplitsRequest;
+import com.amazonaws.athena.connector.lambda.metadata.GetSplitsResponse;
+import com.amazonaws.athena.connector.lambda.metadata.GetTableLayoutRequest;
+import com.amazonaws.athena.connector.lambda.metadata.GetTableLayoutResponse;
+import com.amazonaws.athena.connector.lambda.security.FederatedIdentity;
+import com.amazonaws.connectors.athena.jdbc.TestBase;
+import com.amazonaws.connectors.athena.jdbc.connection.DatabaseConnectionConfig;
+import com.amazonaws.connectors.athena.jdbc.connection.JdbcConnectionFactory;
+import com.amazonaws.connectors.athena.jdbc.connection.JdbcCredentialProvider;
+import com.amazonaws.services.athena.AmazonAthena;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
+import org.apache.arrow.vector.types.pojo.Field;
+import org.apache.arrow.vector.types.pojo.Schema;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class SAPHANAMetadataHandlerTest
+        extends TestBase
+{
+
+    private DatabaseConnectionConfig databaseConnectionConfig = new DatabaseConnectionConfig("testCatalog", JdbcConnectionFactory.DatabaseEngine.SAPHANA,
+            "saphana://jdbc:saphana://hostname/user=A&password=B");
+    private SAPHANAMetadataHandler sapHanaMetadataHandler;
+    private JdbcConnectionFactory jdbcConnectionFactory;
+    private Connection connection;
+    private FederatedIdentity federatedIdentity;
+    private AWSSecretsManager secretsManager;
+    private AmazonAthena athena;
+
+    @Before
+    public void setup()
+    {
+        System.out.println("===================================================");
+        System.out.println("Performing SAP HANA Tests");
+        System.out.println("===================================================");
+        this.jdbcConnectionFactory = Mockito.mock(JdbcConnectionFactory.class);
+        this.connection = Mockito.mock(Connection.class, Mockito.RETURNS_DEEP_STUBS);
+        Mockito.when(this.jdbcConnectionFactory.getConnection(Mockito.any(JdbcCredentialProvider.class))).thenReturn(this.connection);
+        this.secretsManager = Mockito.mock(AWSSecretsManager.class);
+        this.athena = Mockito.mock(AmazonAthena.class);
+        Mockito.when(this.secretsManager.getSecretValue(Mockito.eq(new GetSecretValueRequest().withSecretId("testSecret")))).thenReturn(new GetSecretValueResult().withSecretString("{\"username\": \"testUser\", \"password\": \"testPassword\"}"));
+        this.sapHanaMetadataHandler = new SAPHANAMetadataHandler(databaseConnectionConfig, this.secretsManager, this.athena, this.jdbcConnectionFactory);
+        this.federatedIdentity = Mockito.mock(FederatedIdentity.class);
+    }
+
+    @Test
+    public void getPartitionSchema()
+    {
+        Assert.assertEquals(SchemaBuilder.newBuilder()
+                        .addField(SAPHANAMetadataHandler.BLOCK_PARTITION_COLUMN_NAME, org.apache.arrow.vector.types.Types.MinorType.VARCHAR.getType()).build(),
+                this.sapHanaMetadataHandler.getPartitionSchema("testCatalogName"));
+    }
+
+    @Test
+    public void doGetTableLayout()
+            throws Exception
+    {
+        BlockAllocator blockAllocator = new BlockAllocatorImpl();
+        Constraints constraints = Mockito.mock(Constraints.class);
+        TableName tableName = new TableName("TESTSCHEMA", "TESTTABLE");
+        Schema partitionSchema = this.sapHanaMetadataHandler.getPartitionSchema("testCatalogName");
+        Set<String> partitionCols = partitionSchema.getFields().stream().map(Field::getName).collect(Collectors.toSet());
+        GetTableLayoutRequest getTableLayoutRequest = new GetTableLayoutRequest(this.federatedIdentity, "testQueryId", "testCatalogName", tableName, constraints, partitionSchema, partitionCols);
+
+        PreparedStatement preparedStatement = Mockito.mock(PreparedStatement.class);
+        Mockito.when(this.connection.prepareStatement(SAPHANAMetadataHandler.GET_PARTITIONS_QUERY)).thenReturn(preparedStatement);
+
+        String[] columns = {"part_id"};
+        int[] types = {Types.VARCHAR};
+        Object[][] values = {{"1"}, {"2"}};
+        ResultSet resultSet = mockResultSet(columns, types, values, new AtomicInteger(-1));
+        Mockito.when(preparedStatement.executeQuery()).thenReturn(resultSet);
+
+        Mockito.when(this.connection.getMetaData().getSearchStringEscape()).thenReturn(null);
+
+        GetTableLayoutResponse getTableLayoutResponse = this.sapHanaMetadataHandler.doGetTableLayout(blockAllocator, getTableLayoutRequest);
+
+        Assert.assertEquals(values.length, getTableLayoutResponse.getPartitions().getRowCount());
+
+        List<String> expectedValues = new ArrayList<>();
+        for (int i = 0; i < getTableLayoutResponse.getPartitions().getRowCount(); i++) {
+            expectedValues.add(BlockUtils.rowToString(getTableLayoutResponse.getPartitions(), i));
+        }
+        Assert.assertEquals(expectedValues, Arrays.asList("[part_id : 1]", "[part_id : 2]"));
+
+        SchemaBuilder expectedSchemaBuilder = SchemaBuilder.newBuilder();
+        expectedSchemaBuilder.addField(FieldBuilder.newBuilder(SAPHANAMetadataHandler.BLOCK_PARTITION_COLUMN_NAME, org.apache.arrow.vector.types.Types.MinorType.VARCHAR.getType()).build());
+        Schema expectedSchema = expectedSchemaBuilder.build();
+        Assert.assertEquals(expectedSchema, getTableLayoutResponse.getPartitions().getSchema());
+        Assert.assertEquals(tableName, getTableLayoutResponse.getTableName());
+
+        Mockito.verify(preparedStatement, Mockito.times(1)).setString(1, tableName.getTableName());
+        Mockito.verify(preparedStatement, Mockito.times(1)).setString(2, tableName.getSchemaName());
+    }
+
+    @Test
+    public void doGetTableLayoutWithNoPartitions()
+            throws Exception
+    {
+        BlockAllocator blockAllocator = new BlockAllocatorImpl();
+        Constraints constraints = Mockito.mock(Constraints.class);
+        TableName tableName = new TableName("TESTSCHEMA", "TESTTABLE");
+        Schema partitionSchema = this.sapHanaMetadataHandler.getPartitionSchema("testCatalogName");
+        Set<String> partitionCols = partitionSchema.getFields().stream().map(Field::getName).collect(Collectors.toSet());
+        GetTableLayoutRequest getTableLayoutRequest = new GetTableLayoutRequest(this.federatedIdentity, "testQueryId", "testCatalogName", tableName, constraints, partitionSchema, partitionCols);
+
+        PreparedStatement preparedStatement = Mockito.mock(PreparedStatement.class);
+        Mockito.when(this.connection.prepareStatement(SAPHANAMetadataHandler.GET_PARTITIONS_QUERY)).thenReturn(preparedStatement);
+
+        String[] columns = {"part_id"};
+        int[] types = {Types.VARCHAR};
+        Object[][] values = {{}};
+        ResultSet resultSet = mockResultSet(columns, types, values, new AtomicInteger(-1));
+        Mockito.when(preparedStatement.executeQuery()).thenReturn(resultSet);
+
+        Mockito.when(this.connection.getMetaData().getSearchStringEscape()).thenReturn(null);
+
+        GetTableLayoutResponse getTableLayoutResponse = this.sapHanaMetadataHandler.doGetTableLayout(blockAllocator, getTableLayoutRequest);
+
+        Assert.assertEquals(values.length, getTableLayoutResponse.getPartitions().getRowCount());
+
+        List<String> expectedValues = new ArrayList<>();
+        for (int i = 0; i < getTableLayoutResponse.getPartitions().getRowCount(); i++) {
+            expectedValues.add(BlockUtils.rowToString(getTableLayoutResponse.getPartitions(), i));
+        }
+        Assert.assertEquals(expectedValues, Collections.singletonList("[part_id : *]"));
+
+        SchemaBuilder expectedSchemaBuilder = SchemaBuilder.newBuilder();
+        expectedSchemaBuilder.addField(FieldBuilder.newBuilder(SAPHANAMetadataHandler.BLOCK_PARTITION_COLUMN_NAME, org.apache.arrow.vector.types.Types.MinorType.VARCHAR.getType()).build());
+        Schema expectedSchema = expectedSchemaBuilder.build();
+        Assert.assertEquals(expectedSchema, getTableLayoutResponse.getPartitions().getSchema());
+        Assert.assertEquals(tableName, getTableLayoutResponse.getTableName());
+
+        Mockito.verify(preparedStatement, Mockito.times(1)).setString(1, tableName.getTableName());
+        Mockito.verify(preparedStatement, Mockito.times(1)).setString(2, tableName.getSchemaName());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void doGetTableLayoutWithSQLException()
+            throws Exception
+    {
+        Constraints constraints = Mockito.mock(Constraints.class);
+        TableName tableName = new TableName("TESTSCHEMA", "TESTTABLE");
+        Schema partitionSchema = this.sapHanaMetadataHandler.getPartitionSchema("testCatalogName");
+        Set<String> partitionCols = partitionSchema.getFields().stream().map(Field::getName).collect(Collectors.toSet());
+        GetTableLayoutRequest getTableLayoutRequest = new GetTableLayoutRequest(this.federatedIdentity, "testQueryId", "testCatalogName", tableName, constraints, partitionSchema, partitionCols);
+
+        Connection connection = Mockito.mock(Connection.class, Mockito.RETURNS_DEEP_STUBS);
+        JdbcConnectionFactory jdbcConnectionFactory = Mockito.mock(JdbcConnectionFactory.class);
+        Mockito.when(jdbcConnectionFactory.getConnection(Mockito.any(JdbcCredentialProvider.class))).thenReturn(connection);
+        Mockito.when(connection.getMetaData().getSearchStringEscape()).thenThrow(new SQLException());
+        SAPHANAMetadataHandler sapHanaMetadataHandler = new SAPHANAMetadataHandler(databaseConnectionConfig, this.secretsManager, this.athena, jdbcConnectionFactory);
+
+        sapHanaMetadataHandler.doGetTableLayout(Mockito.mock(BlockAllocator.class), getTableLayoutRequest);
+    }
+
+    @Test
+    public void doGetSplits()
+            throws Exception
+    {
+        BlockAllocator blockAllocator = new BlockAllocatorImpl();
+        Constraints constraints = Mockito.mock(Constraints.class);
+        TableName tableName = new TableName("TESTSCHEMA", "TESTTABLE");
+
+        PreparedStatement preparedStatement = Mockito.mock(PreparedStatement.class);
+        Mockito.when(this.connection.prepareStatement(SAPHANAMetadataHandler.GET_PARTITIONS_QUERY)).thenReturn(preparedStatement);
+
+        String[] columns = {SAPHANAMetadataHandler.PARTITION_COLUMN_NAME};
+        int[] types = {Types.VARCHAR};
+        Object[][] values = {{"1"}, {"2"}};
+        ResultSet resultSet = mockResultSet(columns, types, values, new AtomicInteger(-1));
+        Mockito.when(preparedStatement.executeQuery()).thenReturn(resultSet);
+
+        Mockito.when(this.connection.getMetaData().getSearchStringEscape()).thenReturn(null);
+
+        Schema partitionSchema = this.sapHanaMetadataHandler.getPartitionSchema("testCatalogName");
+        Set<String> partitionCols = partitionSchema.getFields().stream().map(Field::getName).collect(Collectors.toSet());
+        GetTableLayoutRequest getTableLayoutRequest = new GetTableLayoutRequest(this.federatedIdentity, "testQueryId", "testCatalogName", tableName, constraints, partitionSchema, partitionCols);
+
+        GetTableLayoutResponse getTableLayoutResponse = this.sapHanaMetadataHandler.doGetTableLayout(blockAllocator, getTableLayoutRequest);
+
+        BlockAllocator splitBlockAllocator = new BlockAllocatorImpl();
+        GetSplitsRequest getSplitsRequest = new GetSplitsRequest(this.federatedIdentity, "testQueryId", "testCatalogName", tableName, getTableLayoutResponse.getPartitions(), new ArrayList<>(partitionCols), constraints, null);
+        GetSplitsResponse getSplitsResponse = this.sapHanaMetadataHandler.doGetSplits(splitBlockAllocator, getSplitsRequest);
+
+        Set<Map<String, String>> expectedSplits = new HashSet<>();
+        expectedSplits.add(Collections.singletonMap(SAPHANAMetadataHandler.BLOCK_PARTITION_COLUMN_NAME, "1"));
+        expectedSplits.add(Collections.singletonMap(SAPHANAMetadataHandler.BLOCK_PARTITION_COLUMN_NAME, "2"));
+        Assert.assertEquals(expectedSplits.size(), getSplitsResponse.getSplits().size());
+        Set<Map<String, String>> actualSplits = getSplitsResponse.getSplits().stream().map(Split::getProperties).collect(Collectors.toSet());
+        Assert.assertEquals(expectedSplits, actualSplits);
+    }
+
+    @Test
+    public void doGetSplitsContinuation()
+            throws Exception
+    {
+        BlockAllocator blockAllocator = new BlockAllocatorImpl();
+        Constraints constraints = Mockito.mock(Constraints.class);
+        TableName tableName = new TableName("TESTSCHEMA", "TESTTABLE");
+        Schema partitionSchema = this.sapHanaMetadataHandler.getPartitionSchema("testCatalogName");
+        Set<String> partitionCols = partitionSchema.getFields().stream().map(Field::getName).collect(Collectors.toSet());
+        GetTableLayoutRequest getTableLayoutRequest = new GetTableLayoutRequest(this.federatedIdentity, "testQueryId", "testCatalogName", tableName, constraints, partitionSchema, partitionCols);
+
+        PreparedStatement preparedStatement = Mockito.mock(PreparedStatement.class);
+        Mockito.when(this.connection.prepareStatement(SAPHANAMetadataHandler.GET_PARTITIONS_QUERY)).thenReturn(preparedStatement);
+
+        String[] columns = {"part_id"};
+        int[] types = {Types.VARCHAR};
+        Object[][] values = {{"1"}, {"2"}};
+        ResultSet resultSet = mockResultSet(columns, types, values, new AtomicInteger(-1));
+        final String expectedQuery = String.format(SAPHANAMetadataHandler.GET_PARTITIONS_QUERY, tableName.getTableName(), tableName.getSchemaName());
+        Mockito.when(preparedStatement.executeQuery()).thenReturn(resultSet);
+
+        Mockito.when(this.connection.getMetaData().getSearchStringEscape()).thenReturn(null);
+
+        GetTableLayoutResponse getTableLayoutResponse = this.sapHanaMetadataHandler.doGetTableLayout(blockAllocator, getTableLayoutRequest);
+
+        BlockAllocator splitBlockAllocator = new BlockAllocatorImpl();
+        GetSplitsRequest getSplitsRequest = new GetSplitsRequest(this.federatedIdentity, "testQueryId", "testCatalogName", tableName, getTableLayoutResponse.getPartitions(), new ArrayList<>(partitionCols), constraints, "1");
+        GetSplitsResponse getSplitsResponse = this.sapHanaMetadataHandler.doGetSplits(splitBlockAllocator, getSplitsRequest);
+
+        Set<Map<String, String>> expectedSplits = new HashSet<>();
+        expectedSplits.add(Collections.singletonMap("part_id", "2"));
+        Assert.assertEquals(expectedSplits.size(), getSplitsResponse.getSplits().size());
+        Set<Map<String, String>> actualSplits = getSplitsResponse.getSplits().stream().map(Split::getProperties).collect(Collectors.toSet());
+        Assert.assertEquals(expectedSplits, actualSplits);
+    }
+}
