fe819b22090994f70f39c918dc990af524f825ef
Vaquar Khan <vaquar.cna@gmail.com>
2021-12-18 13:27:24 -0600

    Update DynamoDBMetadataHandler.java
    Added missing import

--- athena-dynamodb/src/main/java/com/amazonaws/athena/connectors/dynamodb/DynamoDBMetadataHandler.java
+++ athena-dynamodb/src/main/java/com/amazonaws/athena/connectors/dynamodb/DynamoDBMetadataHandler.java
@@ -69,6 +69,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
