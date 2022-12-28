9373e603de0f8f6b75a307ae55acdd1248c31657
akshay.kachore <akshay.kachore@trianz.com>
2022-12-28 17:31:09 +0530

    adding Unit test cases for GcsMetadataHandler

--- athena-gcs/src/test/java/com/amazonaws/athena/connectors/gcs/GcsSchemaUtilsTest.java
+++ athena-gcs/src/test/java/com/amazonaws/athena/connectors/gcs/GcsSchemaUtilsTest.java
@@ -23,7 +23,12 @@ import com.amazonaws.athena.connectors.gcs.storage.StorageMetadata;
 import com.amazonaws.services.glue.model.StorageDescriptor;
 import com.amazonaws.services.glue.model.Table;
 import com.google.common.collect.ImmutableMap;
+import org.apache.arrow.vector.types.DateUnit;
+import org.apache.arrow.vector.types.TimeUnit;
+import org.apache.arrow.vector.types.pojo.ArrowType;
+import org.apache.arrow.vector.types.pojo.Field;
 import org.apache.arrow.vector.types.pojo.Schema;
+import org.joda.time.tz.UTCProvider;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -43,7 +48,6 @@ import static org.mockito.Mockito.when;
         "javax.management.*", "org.w3c.*", "javax.net.ssl.*", "sun.security.*", "jdk.internal.reflect.*", "javax.crypto.*"
 })
 @PrepareForTest({GcsTestUtils.class})
-
 public class GcsSchemaUtilsTest
 {
     @Mock
@@ -64,4 +68,37 @@ public class GcsSchemaUtilsTest
         assertNotNull(schema);
     }
 
+    @Test(expected = GcsConnectorException.class)
+    public void testBuildTableSchemaException() throws Exception
+    {
+        Table table = new Table();
+        table.setName("birthday");
+        table.setDatabaseName("default");
+        table.setParameters(ImmutableMap.of("classification", "parquet"));
+        table.setStorageDescriptor(new StorageDescriptor()
+                .withLocation("gs://mydatalake1test/birthday/"));
+        table.setCatalogId("catalog");
+        when(storageMetadata.getStorageTable(Mockito.any(),Mockito.any(),Mockito.any())).thenReturn(Optional.empty());
+        Schema schema = GcsSchemaUtils.buildTableSchema(storageMetadata, table);
+        assertNotNull(schema);
+    }
+
+    @Test
+    public void testGetCompatibleField()
+    {
+        Field field = GcsSchemaUtils.getCompatibleField(Field.notNullable("id", new ArrowType.Timestamp(TimeUnit.MICROSECOND, "utc")));
+        assertNotNull(field);
+        Field field1 = GcsSchemaUtils.getCompatibleField(Field.nullable("id", new ArrowType.Timestamp(TimeUnit.MICROSECOND, "utc")));
+        assertNotNull(field1);
+        Field field2 = GcsSchemaUtils.getCompatibleField(Field.notNullable("id", new ArrowType.Binary()));
+        assertNotNull(field2);
+        Field field3 = GcsSchemaUtils.getCompatibleField(Field.nullable("id", new ArrowType.Binary()));
+        assertNotNull(field3);
+    }
+
+    @Test(expected = GcsConnectorException.class)
+    public void testConnectorException()
+    {
+        throw new GcsConnectorException("exception", new Exception());
+    }
 }
