bf5a263a7dce877b6820febae578c7be2e910f0c
Michael Hackett <hackemic@amazon.com>
2023-05-04 11:20:05 -0600

    Remove publish.sh script in favor of the native sam deploy command. Update all references to it across the repo

--- athena-example/src/test/java/com/amazonaws/athena/connectors/example/ExampleUserDefinedFuncHandlerTest.java
+++ athena-example/src/test/java/com/amazonaws/athena/connectors/example/ExampleUserDefinedFuncHandlerTest.java
@@ -62,9 +62,8 @@ public class ExampleUserDefinedFuncHandlerTest
     public void extractTxId()
     {
         if (!enableTests) {
-            //We do this because until you complete the tutorial these tests will fail. When you attempt to publis
-            //using ../toos/publish.sh ...  it will set the publishing flag and force these tests. This is how we
-            //avoid breaking the build but still have a useful tutorial. We are also duplicateing this block
+            //We do this because until you complete the tutorial these tests will fail.
+            //This is how we avoid breaking the build but still have a useful tutorial. We are also duplicateing this block
             //on purpose since this is a somewhat odd pattern.
             logger.info("extractAccount: Tests are disabled, to enable them set the 'publishing' environment variable " +
                     "using maven clean install -Dpublishing=true");
@@ -79,9 +78,8 @@ public class ExampleUserDefinedFuncHandlerTest
     public void decrypt()
     {
         if (!enableTests) {
-            //We do this because until you complete the tutorial these tests will fail. When you attempt to publis
-            //using ../toos/publish.sh ...  it will set the publishing flag and force these tests. This is how we
-            //avoid breaking the build but still have a useful tutorial. We are also duplicateing this block
+            //We do this because until you complete the tutorial these tests will fail.
+            //This is how we avoid breaking the build but still have a useful tutorial. We are also duplicateing this block
             //on purpose since this is a somewhat odd pattern.
             logger.info("extractAccount: Tests are disabled, to enable them set the 'publishing' environment variable " +
                     "using maven clean install -Dpublishing=true");
