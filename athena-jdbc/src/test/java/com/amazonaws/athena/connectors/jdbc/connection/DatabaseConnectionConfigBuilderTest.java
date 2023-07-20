346cb3c5e12006f276b747d36ce044492dd3274f
Andrey Kuzin <akuzin@amazon.com>
2023-07-20 20:51:21 +0000

    Added ! as valid character in Secret, due to syntax of service linked secrets.

--- athena-jdbc/src/test/java/com/amazonaws/athena/connectors/jdbc/connection/DatabaseConnectionConfigBuilderTest.java
+++ athena-jdbc/src/test/java/com/amazonaws/athena/connectors/jdbc/connection/DatabaseConnectionConfigBuilderTest.java
@@ -59,15 +59,6 @@ public class DatabaseConnectionConfigBuilderTest
     @Test(expected = RuntimeException.class)
     public void buildMultipleDatabasesFails()
     {
-        DatabaseConnectionConfig expectedDatabase1 = new DatabaseConnectionConfig("testCatalog1", "mysql",
-                "jdbc:mysql://hostname/${testSecret}", "testSecret");
-        DatabaseConnectionConfig expectedDatabase2 = new DatabaseConnectionConfig("testCatalog2", "postgres",
-                "jdbc:postgresql://hostname/user=testUser&password=testPassword");
-        DatabaseConnectionConfig expectedDatabase3 = new DatabaseConnectionConfig("testCatalog3", "redshift",
-                "jdbc:redshift://hostname:5439/dev?${arn:aws:secretsmanager:us-east-1:1234567890:secret:redshift/user/secret}", "arn:aws:secretsmanager:us-east-1:1234567890:secret:redshift/user/secret");
-        DatabaseConnectionConfig defaultConnection = new DatabaseConnectionConfig("default", "postgres",
-                "jdbc:postgresql://hostname/user=testUser&password=testPassword");
-
         List<DatabaseConnectionConfig> databaseConnectionConfigs = new DatabaseConnectionConfigBuilder()
                 .properties(ImmutableMap.of(
                         "default", CONNECTION_STRING2,
@@ -75,8 +66,6 @@ public class DatabaseConnectionConfigBuilderTest
                         "testCatalog2_connection_string", CONNECTION_STRING2,
                         "testCatalog3_connection_string", CONNECTION_STRING3))
                 .build();
-
-        Assert.assertEquals(Arrays.asList(defaultConnection, expectedDatabase1, expectedDatabase2, expectedDatabase3), databaseConnectionConfigs);
     }
 
     @Test(expected = RuntimeException.class)
@@ -95,5 +84,42 @@ public class DatabaseConnectionConfigBuilderTest
     public void buildMalformedConnectionString()
     {
         new DatabaseConnectionConfigBuilder().properties(Collections.singletonMap("testDb_connection_string", null)).build();
+    }
+
+    @Test
+    public void invalidSecretsSyntaxTest()
+    {
+        String engine = "redshift";
+        List<String> invalidConnectionStrings = Arrays.asList(
+                "redshift://jdbc:redshift://hostname:5439/dev?${inv&li$dSecret}",
+                "redshift://jdbc:redshift://hostname:5439/dev?${an*therOne))}",
+                "redshift://jdbc:redshift://hostname:5439/dev?${in^a]i?}",
+                "redshift://jdbc:redshift://hostname:5439/dev?${an*therOne))}");
+        for (String connection: invalidConnectionStrings) {
+                Assert.assertThrows(RuntimeException.class, () -> new DatabaseConnectionConfigBuilder().properties(Collections.singletonMap("testDb_connection_string", connection)).engine(engine).build());
+        }
+    }
+
+    @Test
+    public void validSecretsSyntaxTest()
+    {
+        String engine = "redshift";
+        String connectionString1 = "redshift://jdbc:redshift://hostname:5439/dev?${spec.@/Ch@rac+=r_}";
+        String connectionString2 = "redshift://jdbc:redshift://hostname:5439/dev?${rds!service-linked-secret}";
+        String connectionString3 = "redshift://jdbc:redshift://hostname:5439/dev?${redshift:credentials-secret}";
+        String connectionString4 = "redshift://jdbc:redshift://hostname:5439/dev?${opsworks-cm:credentials12}";
+        String[] secrets = new String[]{"spec.@/Ch@rac+=r_", "rds!service-linked-secret", "redshift:credentials-secret", "opsworks-cm:credentials12"};
+        List<DatabaseConnectionConfig> databaseConnectionConfigs = new DatabaseConnectionConfigBuilder()
+                .engine(engine)
+                .properties(ImmutableMap.of(
+                        "default", connectionString1,
+                        "testCatalog2_connection_string", connectionString2,
+                        "testCatalog3_connection_string", connectionString3,
+                        "testCatalog4_connection_string", connectionString4))
+                .build();
+        Assert.assertEquals(secrets.length, databaseConnectionConfigs.size());
+        for (int i = 0; i < databaseConnectionConfigs.size(); i++) {
+                Assert.assertEquals(secrets[i], databaseConnectionConfigs.get(i).getSecret());
+        }
     }
 }
