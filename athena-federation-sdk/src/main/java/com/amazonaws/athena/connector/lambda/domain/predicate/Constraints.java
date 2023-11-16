986d8fda3dbadc6e32e07e24985e10874604df91
AbdulRehman Faraj <arfaraj@amazon.com>
2023-11-16 20:14:53 +0000

    Initial QPT commit

--- athena-federation-sdk/src/main/java/com/amazonaws/athena/connector/lambda/domain/predicate/Constraints.java
+++ athena-federation-sdk/src/main/java/com/amazonaws/athena/connector/lambda/domain/predicate/Constraints.java
@@ -22,12 +22,17 @@ package com.amazonaws.athena.connector.lambda.domain.predicate;
 
 import com.amazonaws.athena.connector.lambda.domain.predicate.expression.FederationExpression;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import com.google.common.base.Objects;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Container which holds and maps column names to the corresponding constraint (e.g. ValueSet).
@@ -40,6 +45,7 @@ import java.util.Map;
  * supplying connectors with a more complete view of the query and its predicate. We expect a future release to  provide
  * full predicates to connectors and lets the connector decide which parts of the predicate it is capable of applying
  */
+@JsonIgnoreProperties(value = {"queryPassthroughArguments"}, allowGetters = true, allowSetters = true)
 public class Constraints
         implements AutoCloseable
 {
@@ -49,9 +55,10 @@ public class Constraints
     private List<FederationExpression> expression;
     private final List<OrderByField> orderByClause;
     private long limit;
+    private Map<String, String> queryPassthroughArguments;
 
     @Deprecated
-    public Constraints(Map<String, ValueSet> summary) 
+    public Constraints(Map<String, ValueSet> summary)
     {
         this(summary, Collections.emptyList(), Collections.emptyList(), DEFAULT_NO_LIMIT);
     }
@@ -66,6 +73,7 @@ public class Constraints
         this.expression = expression;
         this.orderByClause = orderByClause;
         this.limit = limit;
+        this.queryPassthroughArguments = Collections.emptyMap();
     }
 
     /**
@@ -103,6 +111,23 @@ public class Constraints
         return this.orderByClause != null && this.orderByClause.size() > 0;
     }
 
+    @JsonGetter("queryPassthroughArguments")
+    public Map<String, String> getQueryPassthroughArguments()
+    {
+        return this.queryPassthroughArguments;
+    }
+
+    @JsonSetter("queryPassthroughArguments")
+    public void setQueryPassthroughArguments(Map<String, String> queryPassthroughArguments)
+    {
+        this.queryPassthroughArguments = requireNonNull(queryPassthroughArguments, "queryPassthroughArguments is null");
+    }
+
+    public boolean isQueryPassThrough()
+    {
+        return !this.queryPassthroughArguments.isEmpty();
+    }
+
     @Override
     public boolean equals(Object o)
     {
@@ -118,7 +143,8 @@ public class Constraints
         return Objects.equal(this.summary, that.summary) &&
                 Objects.equal(this.expression, that.expression) &&
                 Objects.equal(this.orderByClause, that.orderByClause) &&
-                Objects.equal(this.limit, that.limit);
+                Objects.equal(this.limit, that.limit) &&
+                Objects.equal(this.getQueryPassthroughArguments(), that.getQueryPassthroughArguments());
     }
 
     @Override
@@ -129,6 +155,7 @@ public class Constraints
                 "expression=" + expression +
                 "orderByClause=" + orderByClause +
                 "limit=" + limit +
+                "queryPassthroughArguments=" + queryPassthroughArguments +
                 '}';
     }
 
--- athena-federation-sdk/src/main/java/com/amazonaws/athena/connector/lambda/metadata/GetTableRequest.java
+++ athena-federation-sdk/src/main/java/com/amazonaws/athena/connector/lambda/metadata/GetTableRequest.java
@@ -23,18 +23,26 @@ package com.amazonaws.athena.connector.lambda.metadata;
 import com.amazonaws.athena.connector.lambda.domain.TableName;
 import com.amazonaws.athena.connector.lambda.security.FederatedIdentity;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import com.google.common.base.Objects;
+
+import java.util.Collections;
+import java.util.Map;
 
 import static java.util.Objects.requireNonNull;
 
 /**
  * Represents the input of a <code>GetTable</code> operation.
  */
+@JsonIgnoreProperties(value = {"queryPassthroughArguments"}, allowGetters = true, allowSetters = true)
 public class GetTableRequest
         extends MetadataRequest
 {
     private final TableName tableName;
+    private Map<String, String> queryPassthroughArguments;
 
     /**
      * Constructs a new GetTableRequest object.
@@ -53,6 +61,7 @@ public class GetTableRequest
         super(identity, MetadataRequestType.GET_TABLE, queryId, catalogName);
         requireNonNull(tableName, "tableName is null");
         this.tableName = tableName;
+        this.queryPassthroughArguments = Collections.emptyMap();
     }
 
     /**
@@ -72,12 +81,29 @@ public class GetTableRequest
         //No Op
     }
 
+    @JsonGetter("queryPassthroughArguments")
+    public Map<String, String> getQueryPassthroughArguments()
+    {
+        return this.queryPassthroughArguments;
+    }
+
+    @JsonSetter("queryPassthroughArguments")
+    public void setQueryPassthroughArguments(Map<String, String> queryPassthroughArguments)
+    {
+        this.queryPassthroughArguments = requireNonNull(queryPassthroughArguments, "queryPassthroughArguments is null");
+    }
+
+    public boolean isQueryPassthrough()
+    {
+        return !this.queryPassthroughArguments.isEmpty();
+    }
     @Override
     public String toString()
     {
         return "GetTableRequest{" +
                 "queryId=" + getQueryId() +
                 ", tableName=" + tableName +
+                "queryPassthroughArguments=" + queryPassthroughArguments +
                 '}';
     }
 
@@ -95,12 +121,13 @@ public class GetTableRequest
 
         return Objects.equal(this.tableName, that.tableName) &&
                 Objects.equal(this.getRequestType(), that.getRequestType()) &&
-                Objects.equal(this.getCatalogName(), that.getCatalogName());
+                Objects.equal(this.getCatalogName(), that.getCatalogName()) &&
+                Objects.equal(this.getQueryPassthroughArguments(), that.getQueryPassthroughArguments());
     }
 
     @Override
     public int hashCode()
     {
-        return Objects.hashCode(tableName, getRequestType(), getCatalogName());
+        return Objects.hashCode(tableName, getRequestType(), getCatalogName(), getQueryPassthroughArguments());
     }
 }
