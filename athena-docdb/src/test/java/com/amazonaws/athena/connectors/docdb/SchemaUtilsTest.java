99c3679bfc111d72f47ac291665ac3a953c41512
chngpe <102991671+chngpe@users.noreply.github.com>
2023-12-07 15:40:43 -0500

    Enable case insensitive match for docdb on schema and table names (#1639)
    Co-authored-by: ejeffrli <144148373+ejeffrli@users.noreply.github.com>

--- athena-docdb/src/test/java/com/amazonaws/athena/connectors/docdb/SchemaUtilsTest.java
+++ athena-docdb/src/test/java/com/amazonaws/athena/connectors/docdb/SchemaUtilsTest.java
@@ -20,6 +20,7 @@
 package com.amazonaws.athena.connectors.docdb;
 
 import com.amazonaws.athena.connector.lambda.domain.TableName;
+import com.google.common.collect.ImmutableMap;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
@@ -52,11 +53,9 @@ public class SchemaUtilsTest
         unsupported.put("unsupported_col1", new UnsupportedType());
         docs.add(unsupported);
 
-        MongoClient mockClient = mock(MongoClient.class);
         MongoDatabase mockDatabase = mock(MongoDatabase.class);
         MongoCollection mockCollection = mock(MongoCollection.class);
         FindIterable mockIterable = mock(FindIterable.class);
-        when(mockClient.getDatabase(any())).thenReturn(mockDatabase);
         when(mockDatabase.getCollection(any())).thenReturn(mockCollection);
         when(mockCollection.find()).thenReturn(mockIterable);
         when(mockIterable.limit(anyInt())).thenReturn(mockIterable);
@@ -64,7 +63,7 @@ public class SchemaUtilsTest
         when(mockIterable.batchSize(anyInt())).thenReturn(mockIterable);
         when(mockIterable.iterator()).thenReturn(new StubbingCursor(docs.iterator()));
 
-        Schema schema = SchemaUtils.inferSchema(mockClient, new TableName("test", "test"), 10);
+        Schema schema = SchemaUtils.inferSchema(mockDatabase, new TableName("test", "test"), 10);
         assertEquals(1, schema.getFields().size());
 
         Map<String, Field> fields = new HashMap<>();
@@ -115,11 +114,9 @@ public class SchemaUtilsTest
         doc3.put("col5", list);
         docs.add(doc3);
 
-        MongoClient mockClient = mock(MongoClient.class);
         MongoDatabase mockDatabase = mock(MongoDatabase.class);
         MongoCollection mockCollection = mock(MongoCollection.class);
         FindIterable mockIterable = mock(FindIterable.class);
-        when(mockClient.getDatabase(any())).thenReturn(mockDatabase);
         when(mockDatabase.getCollection(any())).thenReturn(mockCollection);
         when(mockCollection.find()).thenReturn(mockIterable);
         when(mockIterable.limit(anyInt())).thenReturn(mockIterable);
@@ -127,7 +124,7 @@ public class SchemaUtilsTest
         when(mockIterable.batchSize(anyInt())).thenReturn(mockIterable);
         when(mockIterable.iterator()).thenReturn(new StubbingCursor(docs.iterator()));
 
-        Schema schema = SchemaUtils.inferSchema(mockClient, new TableName("test", "test"), 10);
+        Schema schema = SchemaUtils.inferSchema(mockDatabase, new TableName("test", "test"), 10);
         assertEquals(6, schema.getFields().size());
 
         Map<String, Field> fields = new HashMap<>();
@@ -167,11 +164,9 @@ public class SchemaUtilsTest
         doc2.put("col4", list2);
         docs.add(doc2);
 
-        MongoClient mockClient = mock(MongoClient.class);
         MongoDatabase mockDatabase = mock(MongoDatabase.class);
         MongoCollection mockCollection = mock(MongoCollection.class);
         FindIterable mockIterable = mock(FindIterable.class);
-        when(mockClient.getDatabase(any())).thenReturn(mockDatabase);
         when(mockDatabase.getCollection(any())).thenReturn(mockCollection);
         when(mockCollection.find()).thenReturn(mockIterable);
         when(mockIterable.limit(anyInt())).thenReturn(mockIterable);
@@ -179,7 +174,7 @@ public class SchemaUtilsTest
         when(mockIterable.batchSize(anyInt())).thenReturn(mockIterable);
         when(mockIterable.iterator()).thenReturn(new StubbingCursor(docs.iterator()));
 
-        Schema schema = SchemaUtils.inferSchema(mockClient, new TableName("test", "test"), 10);
+        Schema schema = SchemaUtils.inferSchema(mockDatabase, new TableName("test", "test"), 10);
         assertEquals(4, schema.getFields().size());
 
         Map<String, Field> fields = new HashMap<>();
