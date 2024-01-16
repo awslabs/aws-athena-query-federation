b3f8c86f703e3b32cfde22b19ce44bb3a0d3b091
AbdulRehman Faraj <arfaraj@amazon.com>
2024-01-16 20:54:38 +0000

    Typo Fix

--- validation_testing/main.py
+++ validation_testing/main.py
@@ -26,9 +26,9 @@ def assert_required_env_vars_set():
         raise RuntimeError("not all expected environment variables were set!")
 
 def pre_test_infra_check():
-    # Responsible for manking sure that all stacks from previous runs are cleaned
+    # Responsible for making sure that all stacks from previous runs are cleaned
     # This is needed as sometimes we have resources that are stuck around after tests
-    # That make the stack deletion impossible, such as ENIs. 
+    # That makes the stack deletion impossible, such as ENIs. 
     # This will ensure that everything is cleaned up before bringing up a new stack
     for connector in TESTABLE_CONNECTORS:
       shell_command = f'sh run_cleanup_infra.sh {connector}'
