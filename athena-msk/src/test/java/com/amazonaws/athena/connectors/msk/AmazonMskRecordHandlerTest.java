c90c9005d156d1b177482d7b1cb5df0f78f00f90
Jithendar Trianz <jithendar.vallepu@trianz.com>
2024-07-05 16:50:21 +0530

    Add support for Avro/Protobuf Data Format in athena-msk Connector

--- athena-msk/src/test/java/com/amazonaws/athena/connectors/msk/AmazonMskRecordHandlerTest.java
+++ athena-msk/src/test/java/com/amazonaws/athena/connectors/msk/AmazonMskRecordHandlerTest.java
@@ -33,13 +33,22 @@ import com.amazonaws.athena.connector.lambda.security.FederatedIdentity;
 import com.amazonaws.athena.connector.lambda.security.LocalKeyFactory;
 import com.amazonaws.athena.connectors.msk.dto.*;
 import com.amazonaws.services.athena.AmazonAthena;
+import com.amazonaws.services.glue.AWSGlue;
+import com.amazonaws.services.glue.AWSGlueClientBuilder;
+import com.amazonaws.services.glue.model.GetSchemaResult;
+import com.amazonaws.services.glue.model.GetSchemaVersionResult;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.DynamicMessage;
+import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchema;
 import org.apache.arrow.vector.types.pojo.Field;
 import org.apache.arrow.vector.types.pojo.FieldType;
 import org.apache.arrow.vector.types.pojo.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.MockConsumer;
 import org.apache.kafka.clients.consumer.OffsetResetStrategy;
@@ -59,6 +68,9 @@ import java.util.HashMap;
 import java.util.UUID;
 
 import static com.amazonaws.athena.connector.lambda.domain.predicate.Constraints.DEFAULT_NO_LIMIT;
+import static com.amazonaws.athena.connectors.msk.AmazonMskConstants.AVRO_DATA_FORMAT;
+import static com.amazonaws.athena.connectors.msk.AmazonMskConstants.PROTOBUF_DATA_FORMAT;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.Mockito.*;
 
@@ -66,6 +78,10 @@ import static org.mockito.Mockito.*;
 public class AmazonMskRecordHandlerTest {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private MockedStatic<AWSGlueClientBuilder> awsGlueClientBuilder;
+
+    @Mock
+    AWSGlue awsGlue;
 
     @Mock
     AmazonS3 amazonS3;
@@ -86,6 +102,8 @@ public class AmazonMskRecordHandlerTest {
     BlockAllocatorImpl allocator;
 
     MockConsumer<String, TopicResultSet> consumer;
+    MockConsumer<String, GenericRecord> avroConsumer;
+    MockConsumer<String, DynamicMessage> protobufConsumer;
     AmazonMskRecordHandler amazonMskRecordHandler;
     private EncryptionKeyFactory keyFactory = new LocalKeyFactory();
     private EncryptionKey encryptionKey = keyFactory.create();
@@ -108,7 +126,16 @@ public class AmazonMskRecordHandlerTest {
             consumer.addRecord(record1);
             consumer.addRecord(record2);
         });
-
+        avroConsumer = new MockConsumer<>(OffsetResetStrategy.EARLIEST);
+        ConsumerRecord<String, GenericRecord> avroRecord = createAvroConsumerRecord("greetings", 0 , "k1", createGenericRecord("greetings"));
+        avroConsumer.schedulePollTask(() -> {
+            avroConsumer.addRecord(avroRecord);
+        });
+        protobufConsumer = new MockConsumer<>(OffsetResetStrategy.EARLIEST);
+        ConsumerRecord<String, DynamicMessage> protobufRecord = createProtobufConsumerRecord("protobuftest", 0, "k1", createDynamicRecord());
+        protobufConsumer.schedulePollTask(() -> {
+            protobufConsumer.addRecord(protobufRecord);
+        });
         spillConfig = SpillConfig.newBuilder()
                 .withEncryptionKey(encryptionKey)
                 //This will be enough for a single block
@@ -123,11 +150,14 @@ public class AmazonMskRecordHandlerTest {
         allocator = new BlockAllocatorImpl();
         mockedMskUtils = Mockito.mockStatic(AmazonMskUtils.class, Mockito.CALLS_REAL_METHODS);
         amazonMskRecordHandler = new AmazonMskRecordHandler(amazonS3, awsSecretsManager, athena, com.google.common.collect.ImmutableMap.of());
+        awsGlueClientBuilder = Mockito.mockStatic(AWSGlueClientBuilder.class);
+        awsGlueClientBuilder.when(()-> AWSGlueClientBuilder.defaultClient()).thenReturn(awsGlue);
     }
 
     @After
     public void close(){
         mockedMskUtils.close();
+        awsGlueClientBuilder.close();
     }
 
     @Test
@@ -147,12 +177,72 @@ public class AmazonMskRecordHandlerTest {
         mockedMskUtils.when(() -> AmazonMskUtils.getKafkaConsumer(schema, com.google.common.collect.ImmutableMap.of())).thenReturn(consumer);
         mockedMskUtils.when(() -> AmazonMskUtils.createSplitParam(anyMap())).thenReturn(splitParameters);
 
+        Mockito.when(awsGlue.getSchema(any())).thenReturn(getSchemaResult());
+        Mockito.when(awsGlue.getSchemaVersion(any())).thenReturn(getJsonSchemaVersionResult());
+
         QueryStatusChecker queryStatusChecker = mock(QueryStatusChecker.class);
         when(queryStatusChecker.isQueryRunning()).thenReturn(true);
 
         ReadRecordsRequest request = createReadRecordsRequest(schema);
         BlockSpiller spiller = new S3BlockSpiller(amazonS3, spillConfig, allocator, schema, ConstraintEvaluator.emptyEvaluator(), com.google.common.collect.ImmutableMap.of());
         amazonMskRecordHandler.readWithConstraint(spiller, request, queryStatusChecker);
+    }
+    @Test
+    public void testForConsumeAvroDataFromTopic() throws Exception {
+        HashMap<TopicPartition, Long> offsets;
+        offsets = new HashMap<>();
+        offsets.put(new TopicPartition("greetings", 0), 0L);
+        avroConsumer.updateBeginningOffsets(offsets);
+
+        offsets = new HashMap<>();
+        offsets.put(new TopicPartition("greetings", 0), 1L);
+        avroConsumer.updateEndOffsets(offsets);
+
+        SplitParameters splitParameters = new SplitParameters("greetings", 0, 0, 1);
+        Schema schema = createAvroSchema(createAvroTopicSchema());
+
+        mockedMskUtils.when(() -> AmazonMskUtils.getAvroKafkaConsumer(com.google.common.collect.ImmutableMap.of())).thenReturn(avroConsumer);
+        mockedMskUtils.when(() -> AmazonMskUtils.createSplitParam(anyMap())).thenReturn(splitParameters);
+
+        Mockito.when(awsGlue.getSchema(any())).thenReturn(getSchemaResult());
+        Mockito.when(awsGlue.getSchemaVersion(any())).thenReturn(getAvroSchemaVersionResult());
+
+        QueryStatusChecker queryStatusChecker = mock(QueryStatusChecker.class);
+        when(queryStatusChecker.isQueryRunning()).thenReturn(true);
+
+        ReadRecordsRequest request = createReadRecordsRequest(schema);
+        BlockSpiller spiller = new S3BlockSpiller(amazonS3, spillConfig, allocator, schema, ConstraintEvaluator.emptyEvaluator(), com.google.common.collect.ImmutableMap.of());
+        amazonMskRecordHandler.readWithConstraint(spiller, request, queryStatusChecker);
+        assertEquals(1, spiller.getBlock().getRowCount());
+    }
+
+    @Test
+    public void testForConsumeProtobufDataFromTopic() throws Exception {
+        HashMap<TopicPartition, Long> offsets;
+        offsets = new HashMap<>();
+        offsets.put(new TopicPartition("protobuftest", 0), 0L);
+        protobufConsumer.updateBeginningOffsets(offsets);
+
+        offsets = new HashMap<>();
+        offsets.put(new TopicPartition("protobuftest", 0), 1L);
+        protobufConsumer.updateEndOffsets(offsets);
+
+        SplitParameters splitParameters = new SplitParameters("protobuftest", 0, 0, 1);
+        Schema schema = createProtobufSchema(createProtobufTopicSchema());
+
+        mockedMskUtils.when(() -> AmazonMskUtils.getProtobufKafkaConsumer(com.google.common.collect.ImmutableMap.of())).thenReturn(protobufConsumer);
+        mockedMskUtils.when(() -> AmazonMskUtils.createSplitParam(anyMap())).thenReturn(splitParameters);
+
+        Mockito.when(awsGlue.getSchema(any())).thenReturn(getSchemaResult());
+        Mockito.when(awsGlue.getSchemaVersion(any())).thenReturn(getProtobufSchemaVersionResult());
+
+        QueryStatusChecker queryStatusChecker = mock(QueryStatusChecker.class);
+        when(queryStatusChecker.isQueryRunning()).thenReturn(true);
+
+        ReadRecordsRequest request = createReadRecordsRequest(schema);
+        BlockSpiller spiller = new S3BlockSpiller(amazonS3, spillConfig, allocator, schema, ConstraintEvaluator.emptyEvaluator(), com.google.common.collect.ImmutableMap.of());
+        amazonMskRecordHandler.readWithConstraint(spiller, request, queryStatusChecker);
+        assertEquals(1, spiller.getBlock().getRowCount());
     }
 
     @Test
@@ -171,6 +261,9 @@ public class AmazonMskRecordHandlerTest {
 
         mockedMskUtils.when(() -> AmazonMskUtils.getKafkaConsumer(schema, com.google.common.collect.ImmutableMap.of())).thenReturn(consumer);
         mockedMskUtils.when(() -> AmazonMskUtils.createSplitParam(anyMap())).thenReturn(splitParameters);
+
+        Mockito.when(awsGlue.getSchema(any())).thenReturn(getSchemaResult());
+        Mockito.when(awsGlue.getSchemaVersion(any())).thenReturn(getJsonSchemaVersionResult());
 
         QueryStatusChecker queryStatusChecker = mock(QueryStatusChecker.class);
         when(queryStatusChecker.isQueryRunning()).thenReturn(false);
@@ -199,6 +292,9 @@ public class AmazonMskRecordHandlerTest {
         mockedMskUtils.when(() -> AmazonMskUtils.getKafkaConsumer(schema, com.google.common.collect.ImmutableMap.of())).thenReturn(consumer);
         mockedMskUtils.when(() -> AmazonMskUtils.createSplitParam(anyMap())).thenReturn(splitParameters);
 
+        Mockito.when(awsGlue.getSchema(any())).thenReturn(getSchemaResult());
+        Mockito.when(awsGlue.getSchemaVersion(any())).thenReturn(getJsonSchemaVersionResult());
+
         ReadRecordsRequest request = createReadRecordsRequest(schema);
         amazonMskRecordHandler.readWithConstraint(null, request, null);
     }
@@ -221,6 +317,9 @@ public class AmazonMskRecordHandlerTest {
 
         mockedMskUtils.when(() -> AmazonMskUtils.getKafkaConsumer(schema, com.google.common.collect.ImmutableMap.of())).thenReturn(consumer);
         mockedMskUtils.when(() -> AmazonMskUtils.createSplitParam(anyMap())).thenReturn(splitParameters);
+
+        Mockito.when(awsGlue.getSchema(any())).thenReturn(getSchemaResult());
+        Mockito.when(awsGlue.getSchemaVersion(any())).thenReturn(getJsonSchemaVersionResult());
 
         QueryStatusChecker queryStatusChecker = mock(QueryStatusChecker.class);
         when(queryStatusChecker.isQueryRunning()).thenReturn(true);
@@ -253,6 +352,14 @@ public class AmazonMskRecordHandlerTest {
         return new ConsumerRecord<>(topic, partition, 0, key, data);
     }
 
+    private ConsumerRecord<String, GenericRecord> createAvroConsumerRecord(String topic, int partition, String key, GenericRecord data) throws Exception {
+        return new ConsumerRecord<>(topic, partition, 0, key, data);
+    }
+
+    private ConsumerRecord<String, DynamicMessage> createProtobufConsumerRecord(String topic, int partition, String key, DynamicMessage data) throws Exception {
+        return new ConsumerRecord<>(topic, partition, 0, key, data);
+    }
+
     private TopicResultSet createTopicResultSet(String topic) {
         TopicResultSet resultSet = new TopicResultSet();
         resultSet.setTopicName(topic);
@@ -263,6 +370,38 @@ public class AmazonMskRecordHandlerTest {
         resultSet.getFields().add(new MSKField("code", "3", "TINYINT", "", Byte.parseByte("101")));
         return resultSet;
     }
+    private GenericRecord createGenericRecord(String topic) {
+        org.apache.avro.Schema.Parser parser = new org.apache.avro.Schema.Parser();
+        String schemaString = "{\"type\": \"record\",\"name\":\"" + topic + "\",\"fields\": [{\"name\": \"id\", \"type\": \"int\"},{\"name\": \"name\", \"type\": \"string\"},{\"name\": \"greeting\",\"type\": \"string\"}]}";
+        org.apache.avro.Schema schema = parser.parse(schemaString);
+        GenericRecord record = new GenericData.Record(schema);
+
+        record.put("id", 1);
+        record.put("name", "John");
+        record.put("greeting", "Hello");
+
+        return record;
+    }
+
+    private DynamicMessage createDynamicRecord() {
+        String schema = "syntax = \"proto3\";\n" +
+                "message protobuftest {\n" +
+                "string name = 1;\n" +
+                "int32 calories = 2;\n" +
+                "string colour = 3; \n" +
+                "}";
+        ProtobufSchema protobufSchema = new ProtobufSchema(schema);
+        Descriptors.Descriptor descriptor = protobufSchema.toDescriptor();
+
+        // Build the dynamic message
+        DynamicMessage.Builder builder = DynamicMessage.newBuilder(descriptor);
+        builder.setField(descriptor.findFieldByName("name"), "cake");
+        builder.setField(descriptor.findFieldByName("calories"), 260);
+        builder.setField(descriptor.findFieldByName("colour"), "white");
+
+        return builder.build();
+    }
+
 
     private Schema createSchema(TopicSchema topicSchema) throws Exception {
         SchemaBuilder schemaBuilder = SchemaBuilder.newBuilder();
@@ -285,6 +424,42 @@ public class AmazonMskRecordHandlerTest {
         return schemaBuilder.build();
     }
 
+    private Schema createAvroSchema(AvroTopicSchema avroTopicSchema) throws Exception {
+        SchemaBuilder schemaBuilder = SchemaBuilder.newBuilder();
+        avroTopicSchema.getFields().forEach(it -> {
+            FieldType fieldType = new FieldType(
+                    true,
+                    AmazonMskUtils.toArrowType(it.getType()),
+                    null,
+                    com.google.common.collect.ImmutableMap.of(
+                            "name", it.getName(),
+                            "formatHint", it.getFormatHint(),
+                            "type", it.getType()
+                    )
+            );
+            Field field = new Field(it.getName(), fieldType, null);
+            schemaBuilder.addField(field);
+        });
+        schemaBuilder.addMetadata("dataFormat", AVRO_DATA_FORMAT);
+        return schemaBuilder.build();
+    }
+
+    private Schema createProtobufSchema(ProtobufSchema protobufTopicSchema) throws Exception {
+        SchemaBuilder schemaBuilder = SchemaBuilder.newBuilder();
+        Descriptors.Descriptor descriptor = protobufTopicSchema.toDescriptor();
+        for (Descriptors.FieldDescriptor fieldDescriptor : descriptor.getFields()) {
+            FieldType fieldType = new FieldType(
+                    true,
+                    AmazonMskUtils.toArrowType(fieldDescriptor.getType().toString()),
+                    null
+            );
+            Field field = new Field(fieldDescriptor.getName(), fieldType, null);
+            schemaBuilder.addField(field);
+        }
+        schemaBuilder.addMetadata("dataFormat", PROTOBUF_DATA_FORMAT);
+        return schemaBuilder.build();
+    }
+
     private TopicSchema createCsvTopicSchema() throws JsonProcessingException {
         String csv = "{" +
                 "\"topicName\":\"test\"," +
@@ -297,5 +472,67 @@ public class AmazonMskRecordHandlerTest {
                 "{\"name\":\"code\",\"type\":\"TINYINT\",\"mapping\":\"3\",\"formatHint\": \"\"}" +
                 "]}}";
         return objectMapper.readValue(csv, TopicSchema.class);
+    }
+
+    private AvroTopicSchema createAvroTopicSchema() throws JsonProcessingException {
+        String avro = "{\"type\": \"record\",\"name\":\"greetings\",\"fields\": [{\"name\": \"id\", \"type\": \"int\"},{\"name\": \"name\", \"type\": \"string\"},{\"name\": \"greeting\",\"type\": \"string\"}]}";
+        return objectMapper.readValue(avro, AvroTopicSchema.class);
+    }
+
+    private ProtobufSchema createProtobufTopicSchema() {
+        String protobuf = "syntax = \"proto3\";\n" +
+                "message protobuftest {\n" +
+                "string name = 1;\n" +
+                "int32 calories = 2;\n" +
+                "string colour = 3; \n" +
+                "}";
+        ProtobufSchema protobufSchema = new ProtobufSchema(protobuf);
+        return protobufSchema;
+    }
+
+    private GetSchemaResult getSchemaResult() {
+
+        String arn = "defaultArn", schemaName = "defaultSchemaName";
+        Long latestSchemaVersion = 123L;
+        GetSchemaResult getSchemaResult = new GetSchemaResult();
+        getSchemaResult.setSchemaArn(arn);
+        getSchemaResult.setSchemaName(schemaName);
+        getSchemaResult.setLatestSchemaVersion(latestSchemaVersion);
+        return getSchemaResult;
+    }
+
+    private GetSchemaVersionResult getJsonSchemaVersionResult() {
+        String arn = "defaultArn", schemaVersionId = "defaultVersionId";
+        GetSchemaVersionResult getJsonSchemaVersionResult = new GetSchemaVersionResult();
+        getJsonSchemaVersionResult.setSchemaArn(arn);
+        getJsonSchemaVersionResult.setSchemaVersionId(schemaVersionId);
+        getJsonSchemaVersionResult.setDataFormat("json");
+        getJsonSchemaVersionResult.setSchemaDefinition("{\"topicName\": \"testtable\", \"\"message\": {\"dataFormat\": \"json\", \"fields\": [{\"name\": \"intcol\", \"mapping\": \"intcol\", \"type\": \"INTEGER\"}]}\"}");
+        return getJsonSchemaVersionResult;
+    }
+
+    private GetSchemaVersionResult getAvroSchemaVersionResult() {
+        String arn = "defaultArn", schemaVersionId = "defaultVersionId";
+        GetSchemaVersionResult getAvroSchemaVersionResult = new GetSchemaVersionResult();
+        getAvroSchemaVersionResult.setSchemaArn(arn);
+        getAvroSchemaVersionResult.setSchemaVersionId(schemaVersionId);
+        getAvroSchemaVersionResult.setDataFormat("avro");
+        getAvroSchemaVersionResult.setSchemaDefinition("{\"type\": \"record\",\"name\":\"greetings\",\"fields\": [{\"name\": \"id\", \"type\": \"int\"},{\"name\": \"name\", \"type\": \"string\"},{\"name\": \"greeting\",\"type\": \"string\"}]}");
+        return getAvroSchemaVersionResult;
+    }
+
+    private GetSchemaVersionResult getProtobufSchemaVersionResult() {
+        String arn = "defaultArn", schemaVersionId = "defaultVersionId";
+        GetSchemaVersionResult getProtobufSchemaVersionResult = new GetSchemaVersionResult();
+        getProtobufSchemaVersionResult.setSchemaArn(arn);
+        getProtobufSchemaVersionResult.setSchemaVersionId(schemaVersionId);
+        getProtobufSchemaVersionResult.setDataFormat("protobuf");
+        getProtobufSchemaVersionResult.setSchemaDefinition("syntax = \"proto3\";\n" +
+                "message protobuftest {\n" +
+                "string name = 1;\n" +
+                "int32 calories = 2;\n" +
+                "string colour = 3; \n" +
+                "}");
+        return getProtobufSchemaVersionResult;
     }
 }
