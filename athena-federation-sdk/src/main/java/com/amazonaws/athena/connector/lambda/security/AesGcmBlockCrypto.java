91a0cedf62ebe29446b3eac51022c6998a2d2896
chngpe <chngpe@amazon.com>
2024-08-19 14:16:16 -0400

    update KMS to AES_256

--- athena-federation-sdk/src/main/java/com/amazonaws/athena/connector/lambda/security/AesGcmBlockCrypto.java
+++ athena-federation-sdk/src/main/java/com/amazonaws/athena/connector/lambda/security/AesGcmBlockCrypto.java
@@ -54,7 +54,7 @@ public class AesGcmBlockCrypto
     private static final Logger logger = LoggerFactory.getLogger(AesGcmBlockCrypto.class);
     protected static final int GCM_TAG_LENGTH_BITS = 16 * 8;
     protected static final int NONCE_BYTES = 12;
-    protected static final int KEY_BYTES = 16;
+    protected static final int KEY_BYTES = 32;
     protected static final String KEYSPEC = "AES";
     protected static final String ALGO = "AES/GCM/NoPadding";
     protected static final String ALGO_BC = "BC";
@@ -123,6 +123,9 @@ public class AesGcmBlockCrypto
         if (key.getKey() == null || key.getKey().length == 0) {
             throw new RuntimeException("Invalid key");
         }
+
+        logger.error("Expected " + NONCE_BYTES + " nonce bytes but found " + key.getNonce().length);
+        logger.error("Expected " + KEY_BYTES + " key bytes but found " + key.getKey().length);
 
         GCMParameterSpec spec = new GCMParameterSpec(GCM_TAG_LENGTH_BITS, key.getNonce());
         SecretKeySpec secretKeySpec = new SecretKeySpec(key.getKey(), KEYSPEC);
