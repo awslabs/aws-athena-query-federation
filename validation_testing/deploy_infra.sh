9853060caf79760e749f38d8aeccd0be6984158b
Aimery Methena <159072740+aimethed@users.noreply.github.com>
2024-10-23 10:40:35 -0400

    update validation_testing to use ECR (#2351)

--- validation_testing/deploy_infra.sh
+++ validation_testing/deploy_infra.sh
@@ -1,10 +1,24 @@
-
 CONNECTOR_NAME=$1
 VALIDATION_TESTING_ROOT=$REPOSITORY_ROOT/validation_testing
 
-# upload connector jar to s3 and update yaml to s3 uri, redirect to /dev/null to not log the s3 path
-aws s3 cp $REPOSITORY_ROOT/athena-$CONNECTOR_NAME/target/athena-$CONNECTOR_NAME-2022.47.1.jar "$S3_JARS_BUCKET/" > /dev/null
-sed -i "s#CodeUri: \"./target/athena-$CONNECTOR_NAME-2022.47.1.jar\"#CodeUri: \"$S3_JARS_BUCKET/athena-$CONNECTOR_NAME-2022.47.1.jar\"#" $REPOSITORY_ROOT/athena-$CONNECTOR_NAME/athena-$CONNECTOR_NAME.yaml
+# ecr repository must be created and an image pushed before the cdk stack is deployed for the lambda to create successfully
+# get the AWS account ID from the current roll (for use in ECR repo name)
+ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
+
+# create the ECR repository
+aws ecr create-repository --repository-name athena-federation-repository-$CONNECTOR_NAME --region us-east-1
+
+# push the ECR image for the connector to the ECR repository
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
+docker build -t athena-federation-repository-$CONNECTOR_NAME $REPOSITORY_ROOT/athena-$CONNECTOR_NAME
+docker tag athena-federation-repository-$CONNECTOR_NAME\:latest $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/athena-federation-repository-$CONNECTOR_NAME\:latest
+docker push $ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/athena-federation-repository-$CONNECTOR_NAME\:latest
+
+# update the template to use the correct ImageUri
+sed -i "s|292517598671|$ACCOUNT_ID|g" "$REPOSITORY_ROOT/athena-$CONNECTOR_NAME/athena-$CONNECTOR_NAME.yaml"
+sed -i "s#\(/athena-federation-repository-$CONNECTOR_NAME:\)[0-9]\{4\}\.[0-9]\{1,2\}\.[0-9]\{1\}#\1latest#" $REPOSITORY_ROOT/athena-$CONNECTOR_NAME/athena-$CONNECTOR_NAME.yaml
+
+echo "FINISHED PUSHING CONNECTOR IMAGE TO ECR REPOSITORY"
 
 # go to cdk dir, build/synth/deploy
 cd $(dirname $(find . -name ATHENA_INFRA_SPINUP_ROOT))/app;
@@ -19,7 +33,5 @@ npm install;
 npm run build;
 npm run cdk synth;
 npm run cdk deploy ${CONNECTOR_NAME}CdkStack > /dev/null;
-
-sed -i "s#CodeUri: \"$S3_JARS_BUCKET/athena-$CONNECTOR_NAME-2022.47.1.jar\"#CodeUri: \"./target/athena-$CONNECTOR_NAME-2022.47.1.jar\"#" $REPOSITORY_ROOT/athena-$CONNECTOR_NAME/athena-$CONNECTOR_NAME.yaml
 
 echo "FINISHED DEPLOYING INFRA FOR ${CONNECTOR_NAME}."
