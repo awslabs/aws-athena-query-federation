31f4705f25e34357dc3085e3c293ee709db32a92
Mario Rial Amado <marmado@amazon.com>
2025-05-05 22:48:51 -0400

    Upgrade Glue Job version to V4_0

--- validation_testing/cdk_federation_infra_provisioning/app/lib/stacks/redshift-stack.ts
+++ validation_testing/cdk_federation_infra_provisioning/app/lib/stacks/redshift-stack.ts
@@ -121,7 +121,7 @@ export class RedshiftStack extends cdk.Stack {
 
     const glueJob = new glue.Job(this, 'redshift_glue_job_create_case_insensitive_data', {
       executable: glue.JobExecutable.pythonShell({
-        glueVersion: glue.GlueVersion.V1_0,
+        glueVersion: glue.GlueVersion.V4_0,
         pythonVersion: glue.PythonVersion.THREE_NINE,
         script: glue.Code.fromAsset(path.join(__dirname, `../../../glue_scripts/redshift_create_case_insensitive_data.py`))
       }),
