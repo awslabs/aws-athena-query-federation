f84587fafe1bca9836dbba1df895b34b45260a48
Chngpe <chngpe@amazon.com>
2025-11-25 12:31:48 -0500

    -add support for specify storage integration -update partition logic for S3Export path -Introduce sql string method in SQL builder, remove duplicate logic in snowflake SQL string builder. -fix query failed when contains predicate -fix query return empty result when multi sources within same query - Use Zero copy for reading export file into our spill file - remove row restriction to a block - fix snowflake where we compute environment vaiable each time we call isS3Enable - add vector converter from Timestamp to datetime milli vector

--- athena-federation-sdk/src/main/java/com/amazonaws/athena/connector/lambda/data/SupportedTypes.java
+++ athena-federation-sdk/src/main/java/com/amazonaws/athena/connector/lambda/data/SupportedTypes.java
@@ -52,6 +52,7 @@ public enum SupportedTypes
     VARCHAR(Types.MinorType.VARCHAR),
     STRUCT(Types.MinorType.STRUCT),
     LIST(Types.MinorType.LIST),
+    TIMESTAMPNTZ(Types.MinorType.TIMESTAMPMILLI),
     MAP(Types.MinorType.MAP);
 
     private Types.MinorType arrowMinorType;
