3f8d27cf3470b4c4aa22188d3f94b2d26e964d23
Leo Yan <leozy@amazon.com>
2026-02-11 15:15:48 -0800

    remove arrow translation

--- athena-federation-sdk/src/main/java/com/amazonaws/athena/connector/substrait/SubstraitAccumulatorVisitor.java
+++ athena-federation-sdk/src/main/java/com/amazonaws/athena/connector/substrait/SubstraitAccumulatorVisitor.java
@@ -19,10 +19,8 @@
  */
 package com.amazonaws.athena.connector.substrait;
 
-import org.apache.arrow.vector.types.FloatingPointPrecision;
-import org.apache.arrow.vector.types.pojo.ArrowType;
-import org.apache.arrow.vector.types.pojo.Field;
-import org.apache.arrow.vector.types.pojo.Schema;
+import org.apache.calcite.rel.type.RelDataType;
+import org.apache.calcite.rel.type.RelDataTypeField;
 import org.apache.calcite.sql.SqlDynamicParam;
 import org.apache.calcite.sql.SqlIdentifier;
 import org.apache.calcite.sql.SqlLiteral;
@@ -34,21 +32,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.Map;
 
 public class SubstraitAccumulatorVisitor extends SqlShuttle
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(SubstraitAccumulatorVisitor.class);
 
     private final List<SubstraitTypeAndValue> accumulator;
-    private final Map<String, String> splitProperties;
-    private final Schema schema;
+    private final RelDataType schema;
     private String currentColumn;
 
-    public SubstraitAccumulatorVisitor(final List<SubstraitTypeAndValue> accumulator, final Map<String, String> splitProperties, final Schema schema)
+    public SubstraitAccumulatorVisitor(final List<SubstraitTypeAndValue> accumulator, final RelDataType schema)
     {
         this.accumulator = accumulator;
-        this.splitProperties = splitProperties;
         this.schema = schema;
     }
 
@@ -70,13 +65,13 @@ public class SubstraitAccumulatorVisitor extends SqlShuttle
             return literal;
         }
 
-        Field arrowField = schema.findField(currentColumn);
+        RelDataTypeField field = schema.getField(currentColumn, true, true);
 
-        if (arrowField == null) {
-            throw new IllegalArgumentException("field " + currentColumn + " not found in " + schema.getFields());
+        if (field == null) {
+            throw new IllegalArgumentException("field " + currentColumn + " not found in schema with fields: " + schema.getFieldNames());
         }
 
-        final SqlTypeName typeName = mapArrowTypeToSqlTypeName(arrowField.getType());
+        final SqlTypeName typeName = field.getType().getSqlTypeName();
         if (literal.getValue() instanceof NlsString) {
             accumulator.add(new SubstraitTypeAndValue(typeName, ((NlsString) literal.getValue()).getValue(), currentColumn));
         }
@@ -84,50 +79,5 @@ public class SubstraitAccumulatorVisitor extends SqlShuttle
             accumulator.add(new SubstraitTypeAndValue(typeName, literal.getValue(), currentColumn));
         }
         return new SqlDynamicParam(0, literal.getParserPosition());
-    }
-
-    private SqlTypeName mapArrowTypeToSqlTypeName(final ArrowType arrowType)
-    {
-        if (arrowType instanceof ArrowType.Int) {
-            final int bitWidth = ((ArrowType.Int) arrowType).getBitWidth();
-            if (bitWidth <= 32) {
-                return SqlTypeName.INTEGER;
-            }
-            return SqlTypeName.BIGINT;
-        }
-        else if (arrowType instanceof ArrowType.FloatingPoint) {
-            final ArrowType.FloatingPoint fp = (ArrowType.FloatingPoint) arrowType;
-            if (fp.getPrecision() == FloatingPointPrecision.SINGLE) {
-                return SqlTypeName.FLOAT;
-            }
-            return SqlTypeName.DOUBLE;
-        }
-        else if (arrowType instanceof ArrowType.Null) {
-            return SqlTypeName.NULL;
-        }
-        else if (arrowType instanceof ArrowType.Utf8 || arrowType instanceof ArrowType.LargeUtf8) {
-            return SqlTypeName.VARCHAR;
-        }
-        else if (arrowType instanceof ArrowType.Bool) {
-            return SqlTypeName.BOOLEAN;
-        }
-        else if (arrowType instanceof ArrowType.Decimal) {
-            return SqlTypeName.DECIMAL;
-        }
-        else if (arrowType instanceof ArrowType.Date) {
-            return SqlTypeName.DATE;
-        }
-        else if (arrowType instanceof ArrowType.Time) {
-            return SqlTypeName.TIME;
-        }
-        else if (arrowType instanceof ArrowType.Timestamp) {
-            return SqlTypeName.TIMESTAMP;
-        }
-        else if (arrowType instanceof ArrowType.Binary || arrowType instanceof ArrowType.LargeBinary) {
-            return SqlTypeName.VARBINARY;
-        }
-        else {
-            return SqlTypeName.VARCHAR;
-        }
     }
 }
