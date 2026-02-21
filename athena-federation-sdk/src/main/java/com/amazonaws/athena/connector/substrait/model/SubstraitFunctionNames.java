2f03bd00dc98964edb503c3420d2561dc5f47b06
Szuyin Lee <suyinlee@amazon.com>
2026-02-21 00:40:18 +0000

    Add units tests and fixes

--- athena-federation-sdk/src/main/java/com/amazonaws/athena/connector/substrait/model/SubstraitFunctionNames.java
+++ athena-federation-sdk/src/main/java/com/amazonaws/athena/connector/substrait/model/SubstraitFunctionNames.java
@@ -37,14 +37,26 @@ public final class SubstraitFunctionNames
     // Comparison operators
     public static final String GT_ANY_ANY = "gt:any_any";
     public static final String GT_PTS_PTS = "gt:pts_pts";
+    public static final String GT_DATE_DATE = "gt:date_date";
+    public static final String GT_TS_TS = "gt:timestamp_timestamp";
     public static final String GTE_ANY_ANY = "gte:any_any";
     public static final String GTE_PTS_PTS = "gte:pts_pts";
+    public static final String GTE_DATE_DATE = "gte:date_date";
+    public static final String GTE_TS_TS = "gte:timestamp_timestamp";
     public static final String LT_ANY_ANY = "lt:any_any";
     public static final String LT_PTS_PTS = "lt:pts_pts";
+    public static final String LT_DATE_DATE = "lt:date_date";
+    public static final String LT_TS_TS = "lt:timestamp_timestamp";
     public static final String LTE_ANY_ANY = "lte:any_any";
     public static final String LTE_PTS_PTS = "lte:pts_pts";
+    public static final String LTE_DATE_DATE = "lte:date_date";
+    public static final String LTE_TS_TS = "lte:timestamp_timestamp";
     public static final String EQUAL_ANY_ANY = "equal:any_any";
+    public static final String EQUAL_DATE_DATE = "equal:date_date";
+    public static final String EQUAL_TS_TS = "equal:timestamp_timestamp";
     public static final String NOT_EQUAL_ANY_ANY = "not_equal:any_any";
+    public static final String NOT_EQUAL_DATE_DATE = "not_equal:date_date";
+    public static final String NOT_EQUAL_TS_TS = "not_equal:timestamp_timestamp";
 
     // Null check operators
     public static final String IS_NULL_ANY = "is_null:any";
