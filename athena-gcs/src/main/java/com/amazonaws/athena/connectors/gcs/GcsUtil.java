d808b86c3332f6f3d801c6b616c1eeb27b89126d
Aashrith Madasu <aashrith19032002@gmail.com>
2026-08-05 13:26:23 -0700

    remove limit pushdown support

--- athena-gcs/src/main/java/com/amazonaws/athena/connectors/gcs/GcsUtil.java
+++ athena-gcs/src/main/java/com/amazonaws/athena/connectors/gcs/GcsUtil.java
@@ -99,9 +99,6 @@ public class GcsUtil
      */
     public static void installGoogleCredentialsJsonFile(java.util.Map<String, String> configOptions, CachableSecretsManager secretsManager) throws IOException
     {
-        if (configOptions.get(GCS_CREDS_SECRET_ARN) == null) {
-            return;
-        }
         String gcsCredentialsJsonString = secretsManager.getSecret(
             configOptions.get(GCS_CREDS_SECRET_ARN));
         File destination = new File(GOOGLE_SERVICE_ACCOUNT_JSON_TEMP_FILE_LOCATION_VALUE);
